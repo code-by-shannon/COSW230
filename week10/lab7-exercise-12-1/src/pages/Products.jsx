57225329c2cc1df245564bc84fd39219533251ce
Shannon <sestumpf@gmail.com>
2026-05-19 14:31:15 -0700

    ajax assignment lab 8

--- week10/lab7-exercise-12-1/src/pages/Products.jsx
+++ week10/lab7-exercise-12-1/src/pages/Products.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
+
 export default function products(){
+    const [products, setProducts] = useState([])
+
+    fetch('/products.json')
+    .then(response => response.json())
+    .then(data => setProducts(data.products));
+
+    const myProducts = 
+    products.map((product) => {
+    return (
+    <>
+        <p>{product.name}</p>
+        <p>{product.description}</p>
+    </>
+    )
+    })
+    
     return(
         <>
-            <p>Guitars</p>
-            <p>Amps</p>
-            <p>Pedals</p>
-            <p>Microphones</p>
-            <p>PA System</p>
+            { myProducts }
         </>
     )
 }
